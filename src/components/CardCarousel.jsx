690cbea4fb877e0f0cb977178fdedfd709b1f0c9
Sabari Narayana <sabarinarayana@Sabaris-MacBook-Air.local>
2024-03-03 11:08:04 +0530

    carousel completed

--- src/components/CardCarousel.jsx
+++ src/components/CardCarousel.jsx
@@ -10,22 +10,26 @@ import { data } from "../data/courseData";
 import { CourseCard } from "./CourseCard";
 
 export default function CardCarousel() {
-  console.log(data);
   return (
-    <Carousel className="">
-      <CarouselContent>
-        {data.map((card, index) => (
-          <div className="pl-40" key={index}>
-            <CourseCard
-              image={card.image}
-              CourseName={card.courseName}
-              enrollNo={card.enrollNo}
-            />
-          </div>
-        ))}
-      </CarouselContent>
-      <CarouselPrevious />
-      <CarouselNext />
-    </Carousel>
+    <div className=" flex justify-center">
+      <Carousel className="w-4/5">
+        <CarouselContent>
+          {data.map((card, index) => (
+            <CarouselItem
+              className="basis-1/4 text-center text-white"
+              key={index}
+            >
+              <CourseCard
+                image={card.image}
+                CourseName={card.courseName}
+                enrollNo={card.enrollNo}
+              />
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+    </div>
   );
 }
